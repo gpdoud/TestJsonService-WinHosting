9040e6adcbb2333a7fd88b21ef1fc64e55c4c49b
Greg Doud <gpdoud@users.noreply.github.com>
2019-12-25 08:32:42 -0500

    Add project files.

---
+++ TestJsonService/Controllers/SystemController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestJsonService.Controllers {
+    [Route("[controller]")]
+    [ApiController]
+    public class SystemController : ControllerBase {
+
+        public SystemController() { }
+
+        [HttpGet]
+        public IActionResult GetStatus() {
+            return new OkObjectResult("Status: Test Service is running fine.");
+        }
+    }
+}
